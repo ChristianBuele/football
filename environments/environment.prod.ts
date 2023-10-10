9c247a49c202e774a88bf6aa1109c3ad1aa60b4f
christianbuele <christian97.b@hotmail.es>
2023-10-09 20:46:44 -0500

    size

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl:'http://127.0.0.1:8000/api',
-    SOCKET_ENDPOINT:'http://127.0.0.1:8000'
+    baseUrl:'https://topemos.onrender.com/api',
+    SOCKET_ENDPOINT:'https://topemos.onrender.com:8000'
     
   };
